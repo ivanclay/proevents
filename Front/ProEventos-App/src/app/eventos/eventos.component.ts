d665179f7cdc1a4bd8b3434cda212f0264ab56d9
ivanclay <ivan.clay.moura@gmail.com>
2021-11-15 14:26:55 -0300

    Continuando...

--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -1,5 +1,11 @@
-import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef } from '@angular/core';
+
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { ToastrService } from 'ngx-toastr';
+
+import { Evento } from '../models/Evento';
+import { EventoService } from '../services/evento.service';
 
 @Component({
   selector: 'app-eventos',
@@ -8,8 +14,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventosComponent implements OnInit {
 
-  public eventos: any = [];
-  public eventosFiltrados: any = [];
+  modalRef?: BsModalRef;
+
+  public eventos: Evento[] = [];
+  public eventosFiltrados: Evento[] = [];
 
   widthImg: number = 150;
   marginImg: number = 2;
@@ -25,7 +33,7 @@ export class EventosComponent implements OnInit {
     this.eventosFiltrados = this.filtroLista ? this.filtrarEventos(this.filtroLista) : this.eventos;
   }
 
-  filtrarEventos(filtroLista: string): any {
+  public filtrarEventos(filtroLista: string): Evento[] {
     filtroLista = filtroLista.toLocaleLowerCase();
     return this.eventos.filter(
       (evento: any) =>
@@ -34,19 +42,42 @@ export class EventosComponent implements OnInit {
     );
   }
 
-  constructor(private http: HttpClient) { }
+  constructor(
+    private eventoService: EventoService,
+    private modalService: BsModalService,
+    private toastr: ToastrService,
+    private spinner: NgxSpinnerService
+  ) { }
 
   ngOnInit(): void {
+    this.spinner.show();
     this.getEventos();
   }
 
   public getEventos():void {
-    this.http.get('https://localhost:5001/api/eventos').subscribe(
-      response => {
+    this.eventoService.getEventos().subscribe({
+      next: (response: Evento[]) => {
         this.eventos = response
         this.eventosFiltrados = this.eventos
       },
-      error => console.log(error)
-    );
+      error: (error: any) => {
+        this.spinner.hide();
+        this.toastr.error('Erro ao carregar eventos', 'Erro!');
+      },
+      complete: () => this.spinner.hide()
+    });
+  }
+
+  openModal(template: TemplateRef<any>) : void {
+    this.modalRef = this.modalService.show(template, {class: 'modal-sm'});
+  }
+
+  confirm(): void {
+    this.modalRef?.hide();
+    this.toastr.success('O evento foi excluído com sucesso.', 'Excluído!');
+  }
+
+  decline(): void {
+    this.modalRef?.hide();
   }
 }
