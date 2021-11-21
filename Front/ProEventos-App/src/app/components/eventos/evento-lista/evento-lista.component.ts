b96c091ab13382a85df0ada0316d502d6d8b49be
ivanclay <ivan.clay.moura@gmail.com>
2021-11-21 14:03:10 -0300

    Configurando PATH no tsconfig.json para '@app/'

--- Front/ProEventos-App/src/app/components/eventos/evento-lista/evento-lista.component.ts
+++ Front/ProEventos-App/src/app/components/eventos/evento-lista/evento-lista.component.ts
@@ -4,9 +4,12 @@ import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { ToastrService } from 'ngx-toastr';
 
-import { Evento } from '../../../models/Evento';
-import { EventoService } from '../../../services/evento.service';
-import { TituloComponent } from '../../../shared/titulo/titulo.component';
+import { Evento } from '@app/models/Evento';
+import { EventoService } from '@app/services/evento.service';
+
+// import { Evento } from '../../../models/Evento';
+// import { EventoService } from '../../../services/evento.service';
+// import { TituloComponent } from '../../../shared/titulo/titulo.component';
 
 @Component({
   selector: 'app-evento-lista',
